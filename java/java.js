33ca9db608cd8355be41690389cd17faaa63c2cc
AlexPrada05 <babauprada11@gmail.com>
2024-11-20 20:49:03 +0100

    RELOJ HORA ACTUAL (en los 3 archivos)

--- java/java.js
+++ java/java.js
@@ -1,1 +1,31 @@
+let nombre = 'Alex';
+let apellido = 'Prada';
+let saludo = nombre.concat(apellido);
 
+document.write('Nombre: ' + nombre);
+document.write('<br>');
+document.write('Apellido: ' + apellido);
+document.write('<br>');
+document.write(saludo);
+
+function currenTime() {
+    //CurrenTime se refiere a hora actual.
+    let date = new Date();
+    // aquí declaramos hh para horas; mm para minutos; y ss para segundos.
+    //Las obtemos gracias al date.GetHours, (nos da la hora).
+    //Las obtemos gracias al date.GetMinutes, (nos da los minutos).
+    //Las obtemos gracias al date.GetSeconds, (nos da loss segundos).
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh == (hh < 10) ? "0" + hh : hh;
+    mm == (mm < 10) ? "0" + mm : mm;
+    ss == (ss < 10) ? "0" + ss : ss;
+
+    let time = (hh + ":" + mm + ":" + ss);
+    // esto es lo que hacer verse la hora.
+    let horaActual = document.querySelector('#horaActual');
+    horaActual.innerHTML = time;
+}
+setInterval(currenTime, 1000);
